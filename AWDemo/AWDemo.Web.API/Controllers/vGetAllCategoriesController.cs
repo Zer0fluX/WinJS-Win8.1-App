231be12fde3cf075ff69d7d3a9a6db8ca1fef144
Eric Dzikowski <Zer0fluX@users.noreply.github.com>
2015-11-19 16:59:56 -0600

    Added WebAPI project, data models, and a few controllers.

---
+++ AWDemo/AWDemo.Web.API/Controllers/vGetAllCategoriesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AWDemo.Web.API.Models;
+
+namespace AWDemo.Web.API.Controllers
+{
+    public class vGetAllCategoriesController : ApiController
+    {
+        private AWDemoContext db = new AWDemoContext();
+
+        // GET: api/vGetAllCategories
+        public IQueryable<vGetAllCategory> GetvGetAllCategories()
+        {
+            return db.vGetAllCategories;
+        }
+
+        // GET: api/vGetAllCategories/5
+        [ResponseType(typeof(vGetAllCategory))]
+        public IHttpActionResult GetvGetAllCategory(string id)
+        {
+            vGetAllCategory vGetAllCategory = db.vGetAllCategories.Find(id);
+            if (vGetAllCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vGetAllCategory);
+        }
+
+        // PUT: api/vGetAllCategories/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutvGetAllCategory(string id, vGetAllCategory vGetAllCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vGetAllCategory.ParentProductCategoryName)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vGetAllCategory).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!vGetAllCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/vGetAllCategories
+        [ResponseType(typeof(vGetAllCategory))]
+        public IHttpActionResult PostvGetAllCategory(vGetAllCategory vGetAllCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.vGetAllCategories.Add(vGetAllCategory);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (vGetAllCategoryExists(vGetAllCategory.ParentProductCategoryName))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = vGetAllCategory.ParentProductCategoryName }, vGetAllCategory);
+        }
+
+        // DELETE: api/vGetAllCategories/5
+        [ResponseType(typeof(vGetAllCategory))]
+        public IHttpActionResult DeletevGetAllCategory(string id)
+        {
+            vGetAllCategory vGetAllCategory = db.vGetAllCategories.Find(id);
+            if (vGetAllCategory == null)
+            {
+                return NotFound();
+            }
+
+            db.vGetAllCategories.Remove(vGetAllCategory);
+            db.SaveChanges();
+
+            return Ok(vGetAllCategory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool vGetAllCategoryExists(string id)
+        {
+            return db.vGetAllCategories.Count(e => e.ParentProductCategoryName == id) > 0;
+        }
+    }
+}
---
+++ AWDemo/AWDemo.Web.API/Controllers/vProductAndDescriptionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AWDemo.Web.API.Models;
+
+namespace AWDemo.Web.API.Controllers
+{
+    public class vProductAndDescriptionsController : ApiController
+    {
+        private AWDemoContext db = new AWDemoContext();
+
+        // GET: api/vProductAndDescriptions
+        public IQueryable<vProductAndDescription> GetvProductAndDescriptions()
+        {
+            return db.vProductAndDescriptions;
+        }
+
+        // GET: api/vProductAndDescriptions/5
+        [ResponseType(typeof(vProductAndDescription))]
+        public IHttpActionResult GetvProductAndDescription(int id)
+        {
+            vProductAndDescription vProductAndDescription = db.vProductAndDescriptions.Find(id);
+            if (vProductAndDescription == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vProductAndDescription);
+        }
+
+        // PUT: api/vProductAndDescriptions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutvProductAndDescription(int id, vProductAndDescription vProductAndDescription)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vProductAndDescription.ProductID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vProductAndDescription).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!vProductAndDescriptionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/vProductAndDescriptions
+        [ResponseType(typeof(vProductAndDescription))]
+        public IHttpActionResult PostvProductAndDescription(vProductAndDescription vProductAndDescription)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.vProductAndDescriptions.Add(vProductAndDescription);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (vProductAndDescriptionExists(vProductAndDescription.ProductID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = vProductAndDescription.ProductID }, vProductAndDescription);
+        }
+
+        // DELETE: api/vProductAndDescriptions/5
+        [ResponseType(typeof(vProductAndDescription))]
+        public IHttpActionResult DeletevProductAndDescription(int id)
+        {
+            vProductAndDescription vProductAndDescription = db.vProductAndDescriptions.Find(id);
+            if (vProductAndDescription == null)
+            {
+                return NotFound();
+            }
+
+            db.vProductAndDescriptions.Remove(vProductAndDescription);
+            db.SaveChanges();
+
+            return Ok(vProductAndDescription);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool vProductAndDescriptionExists(int id)
+        {
+            return db.vProductAndDescriptions.Count(e => e.ProductID == id) > 0;
+        }
+    }
+}
